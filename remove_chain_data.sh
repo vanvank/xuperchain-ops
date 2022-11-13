e4bbfdf238d2dda4f8ffb64f607258b9d13804db
Zhicheng <165584943@qq.com>
2022-11-13 23:37:06 +0800

    曾加删除链数据的脚本

---
+++ remove_chain_data.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd $CHAIN_BASE_DIR
+end=$1
+
+if [[ -z $1 ]];then
+echo -n "请输入node num, 比如要操作前3个node,就输入3: "
+read end
+fi
+
+for i in $(seq 2 $end)
+do
+	rm -rf node$/data/blockchain
+done
+
+echo "保险起见，第一个节点的data未删，请手动删除"
+echo "手动执行: rm -rf $CHAIN_BASE_DIR/node1/data/blockchain"
